ae0f227911881dc1656d1b452a5276c833a1c4a0
minjung0067 <minjung0067@naver.com>
2024-04-13 16:21:06 +0900

    add: add xcode proj

---
+++ TToMe/TToMe/TToMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TToMeApp.swift
+//  TToMe
+//
+//  Created by Minjung Lee on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TToMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
